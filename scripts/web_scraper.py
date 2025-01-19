9829e36fcaac774b35f933eb8ad78f02c50eb85d
Ashley <jwang745@usc.edu>
2025-01-19 02:41:12 +0000

    modified web_scraper.py

--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -1,13 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+# from requests_html import HTMLSession
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--user-data-dir=/tmp/selenium_user_data")
+
+service = Service('/usr/bin/chromedriver')
+driver = webdriver.Chrome(service = service, options = chrome_options)
+# session = HTMLSession()
 url = " https://www.cnbc.com/world/?region=world"
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-pretty_html = soup.prettify()
+# session.browser_args = {'executablePath': '/usr/bin/chromium-brower'}
+# r = session.get(url)
+# r.html.render()
+# html = r.html.raw_html
+# soup = BeautifulSoup(html, "html.parser")
+driver.get(url)
+html = driver.page_source
 
 raw_data_path = "../data/raw_data/web_data.html"
-with open(raw_data_path, "w") as file:
-	file.write(pretty_html)
+with open(raw_data_path, "w", encoding = 'utf-8') as file:
+	# file.write(soup.prettify())
+	file.write(html)
+driver.quit()
 
